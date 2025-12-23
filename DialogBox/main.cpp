76cf2646c3523df084c8b715831508baa3e13f10
S1iM <S1iM@HOME-PC>
2025-12-23 21:52:44 +0400

    Login invitation DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,13 +1,17 @@
 ﻿#include<Windows.h>
-#include <CommCtrl.h> 
 #include"resource.h"
 
+CONST CHAR g_sz_INVITE[] = "Введите имя пользователя";
+		//g_sz_ - Global StringZero;
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+		//hwnd – окно; handler to Window(Дескриптор, описатель окна).
+		//uMsg – сообщение;
+		//wParam, lParam – параметры сообщения, самые обычные значения типа DWORD(INT).
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
-	
+
 	return 0;
 }
 
@@ -20,18 +24,38 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1)); // Загружаем иконку
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon); // Кому отправляем
 		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), EM_SETCUEBANNER, FALSE, (LPARAM)"Введите имя пользователя");
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
 	}
 	return TRUE;
-		break;
-	case WM_COMMAND:
+	break;
+	case WM_COMMAND:	//В этой секции обрабатываются нажатия кнопок, клавиш и другие события
 		switch (LOWORD(wParam))
 		{
-		case IDC_BUTTON_COPY: 
+		case IDC_EDIT_LOGIN:
 		{
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE] = {};
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN); 
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			//hwnd - родительское окно; IDC_EDIT_LOGIN - ResourceID элемента, дескриптор которого мы хотим поллучить
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				if (strcmp(sz_buffer, g_sz_INVITE) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				if (strcmp(sz_buffer, "") == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
+			}
+		}
+		break;
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
 			//Функция GetDlgItem() позволяет получить HWND элемент окна 'hwnd', по RESOURCE_ID нужного элемента.
 			//RESOURCE_ID - это самое обычное значение элемента типа 'INT'
 			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
