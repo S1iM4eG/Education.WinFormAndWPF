57636a7d68277ec7064cf5020d6001b43c6c6a29
S1iM <S1iM@HOME-PC>
2025-12-25 15:24:04 +0400

    Solution HW ComboBox

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -3,6 +3,7 @@
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "Combo", "Box" };
 
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -17,6 +18,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
+		HICON hIсon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIсon);
+	}
+	{
 		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
 		for (int i = 0; i < sizeof(g_sz_VALUES) / sizeof(g_sz_VALUES[0]); i++)
 		{
@@ -25,9 +30,36 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int index = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+
+			if (index != CB_ERR)
+			{
+				char itemText[64], result[64];
+
+				SendMessage(hCombo, CB_GETLBTEXT, index, (LPARAM)itemText);
+				wsprintf(result, "Выбран элемент №%d со значением: %s",	index + 1, itemText);
+				MessageBox(hwnd, result, "Информационное окно", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hwnd, "Ни один из элементов не был выбран.", "Информационное окно", MB_OK | MB_ICONERROR);
+			}
+			EndDialog(hwnd, 0);
+		}
+		break;
+
+		case IDCANCEL: 
+			MessageBox(hwnd, "Была нажата кнопка \"CANCEL\"", "Информационное окно", MB_OK | MB_ICONSTOP);
+			EndDialog(hwnd, 0);
+		}
 		break;
 	case WM_CLOSE:
-		EndDialog(hwnd,0);
+		EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
