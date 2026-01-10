1999b0b80f1a2a08284c8d51e572ffc182745f8e
S1iM <S1iM@HOME-PC>
2026-01-10 17:30:51 +0400

    CreatingWindows DONE

--- MainWindows/main.cpp
+++ MainWindows/main.cpp
@@ -76,6 +76,49 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+	{
+		HWND hStatic = CreateWindowEx
+		(
+			NULL,		//exStyle
+			"Static",   //Window class
+			"Этот текст создан функцией CreateWindowEx()",   //Window text SendMessage(hwnd, WM_SETTEXT, .....)
+			WS_CHILD | WS_VISIBLE,   //Windows styles
+			10, 10,		//Position
+			500, 25,	//Size
+			hwnd,		//Parent Window
+			(HMENU)1000, //ResourceID
+			GetModuleHandle(NULL), //hInstance
+			NULL
+		);
+		HWND hEdit = CreateWindowEx
+		(
+			NULL,
+			"Edit",
+			"",
+			WS_CHILD | WS_VISIBLE | WS_BORDER,
+			10, 40,
+			500, 20,
+			hwnd,
+			(HMENU)1001,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		//100 ....999 - Окна, значки
+		//1000 ....40000 - Элементы управления окном
+		CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"Применить",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			410, 70,
+			100, 25,
+			hwnd,
+			(HMENU)1002,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
 		break;
 	case WM_SIZE:
 	case WM_MOVE:
@@ -93,6 +136,20 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	break;
 	case WM_COMMAND:
+	
+		switch (LOWORD(wParam))
+		{
+		case 1002:
+		{
+			CHAR sz_buffer[MAX_PATH] = {};
+			HWND hStatic = GetDlgItem(hwnd, 1000);
+			HWND hEdit = GetDlgItem(hwnd, 1001);
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
+		}
+	
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
